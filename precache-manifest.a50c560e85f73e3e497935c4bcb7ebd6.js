5186bde563091621b9031ebe2cc3d228cfa0b7ef
Matthew Bower <mttbwr91@gmail.com>
2023-01-14 17:49:31 -0800

    Updates

---
+++ precache-manifest.a50c560e85f73e3e497935c4bcb7ebd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bf42310f7cac2d59494",
+    "url": "/travel-react/static/css/main.f4f757f4.chunk.css"
+  },
+  {
+    "revision": "9bf42310f7cac2d59494",
+    "url": "/travel-react/static/js/main.9bf42310.chunk.js"
+  },
+  {
+    "revision": "843f044ab479648ba9c3",
+    "url": "/travel-react/static/js/1.843f044a.chunk.js"
+  },
+  {
+    "revision": "134e8eaac35ae672ccff",
+    "url": "/travel-react/static/js/runtime~main.134e8eaa.js"
+  },
+  {
+    "revision": "38aa4609a5470792933e00a8738fbf66",
+    "url": "/travel-react/index.html"
+  }
+];
